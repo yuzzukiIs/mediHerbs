067e39f532de7b18cb6b7b5ecc2ba5a6152e69cd
Yuzzuki <yuzzuki@bk.ru>
2022-12-12 12:36:45 +0600

    mediHerbsProto3

--- medi-herbs-app/src/pages/PaymentPage/PaymentPage.jsx
+++ medi-herbs-app/src/pages/PaymentPage/PaymentPage.jsx
@@ -6,19 +6,35 @@ const PaymentPage = () => {
             <div className="paymentPage__block">
                 <div className="paymentPage__block__container">
                     <div className="rowBlock">
-
+                        <div className="rowBlock-paymentSum">
+                            сумма
+                        </div>
+                        <select className="rowBlock-paymentSelect">
+                            <option>KGS</option>
+                            <option>RUB</option>
+                            <option>KZT</option>
+                            <option>USD</option>
+                            <option>EUR</option>
+                        </select>
+                        <div className="rowBlock-paymentEmail">
+                            Email адрес...
+                        </div>
                     </div>
-                    <div className="rowBlock-comment">
-
-                    </div>
+                    <input className="rowBlock-comment" type="text" placeholder="Комментарий...">
+                        
+                    </input>
                     <div className="rowBlock-paymentMethod__title">
                         Способ оплаты
                     </div>
-                    <div className="rowBlock-paymentMethod">
-
-                    </div>
+                    <select className="rowBlock-paymentMethod">
+                        <option>Банковской картой</option>
+                        <option>MegaPay</option>
+                        <option>O деньги</option>
+                        <option>Мбанк</option>
+                        <option>Элсом</option>
+                    </select>
                     <div className="rowBlock-acceptPolicy">
-                        <input type="checkbox"></input>Я принимаю условия<a href="#" target="_blank" rel="norefferer">Публичной оферты</a>
+                        <input type="checkbox"></input>Я принимаю условия <a href="#" target="_blank" rel="norefferer">Публичной оферты</a>
                     </div>
                     <buttonn className="rowBlock-paymentButton">
                         Выполнить
